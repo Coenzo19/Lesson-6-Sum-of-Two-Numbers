d4ff2be38938b7175e4db0b0c7691451dcd78a86
Coenzo19 <renzoromero350@gmail.com>
2026-02-05 03:28:49 +0000

    make Sum function

--- sum.js
+++ sum.js
@@ -1,10 +1,10 @@
 // Write a function called `sum` that returns the sum of two numbers
-export function sum(a, b) {
-  // fill me in!
+function sum(a, b) {
+  return a+b;
 }
 
 // Call the sum function (try doing this yourself!)
-
+sum(12,5);
 
 // After you've tried your function with different inputs, comment out any
 // lines that print to the console in this file and run the tests!
