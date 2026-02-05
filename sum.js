47f4ec870ee6f06b828e3d45b7072826c709d3ad
CodeManMalck <51975207+CodeManMalck@users.noreply.github.com>
2026-02-04 17:07:43 -0800

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,10 @@
+// Write a function called `sum` that returns the sum of two numbers
+export function sum(a, b) {
+  // fill me in!
+}
+
+// Call the sum function (try doing this yourself!)
+
+
+// After you've tried your function with different inputs, comment out any
+// lines that print to the console in this file and run the tests!
